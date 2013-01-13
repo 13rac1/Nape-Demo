2fe8e3b7487ee37b6df3cc702472f835c13d4869
Brad Erickson <eosrei@gmail.com>
2013-01-12 19:49:56 -0800

    Remove the physaxe reference

---
+++ bin/flash/haxe/ApplicationMain.hx
@@ -0,0 +1,154 @@
+#if nme
+
+import Main;
+import nme.Assets;
+import nme.events.Event;
+
+
+class ApplicationMain {
+	
+	static var mPreloader:NMEPreloader;
+
+	public static function main () {
+		
+		var call_real = true;
+		
+		
+		var loaded:Int = nme.Lib.current.loaderInfo.bytesLoaded;
+		var total:Int = nme.Lib.current.loaderInfo.bytesTotal;
+		
+		nme.Lib.current.stage.align = nme.display.StageAlign.TOP_LEFT;
+		nme.Lib.current.stage.scaleMode = nme.display.StageScaleMode.NO_SCALE;
+		
+		if (loaded < total || true) /* Always wait for event */ {
+			
+			call_real = false;
+			mPreloader = new NMEPreloader();
+			nme.Lib.current.addChild(mPreloader);
+			mPreloader.onInit();
+			mPreloader.onUpdate(loaded,total);
+			nme.Lib.current.addEventListener (nme.events.Event.ENTER_FRAME, onEnter);
+			
+		}
+		
+		
+		
+		
+		
+		
+
+		if (call_real)
+			begin ();
+	}
+
+	
+	
+	private static function begin () {
+		
+		var hasMain = false;
+		
+		for (methodName in Type.getClassFields(Main))
+		{
+			if (methodName == "main")
+			{
+				hasMain = true;
+				break;
+			}
+		}
+		
+		if (hasMain)
+		{
+			Reflect.callMethod (Main, Reflect.field (Main, "main"), []);
+		}
+		else
+		{
+			var instance = Type.createInstance(Main, []);
+			if (Std.is (instance, nme.display.DisplayObject)) {
+				nme.Lib.current.addChild(cast instance);
+			}	
+		}
+		
+	}
+
+	static function onEnter (_) {
+		
+		var loaded = nme.Lib.current.loaderInfo.bytesLoaded;
+		var total = nme.Lib.current.loaderInfo.bytesTotal;
+		mPreloader.onUpdate(loaded,total);
+		
+		if (loaded >= total) {
+			
+			nme.Lib.current.removeEventListener(nme.events.Event.ENTER_FRAME, onEnter);
+			mPreloader.addEventListener (Event.COMPLETE, preloader_onComplete);
+			mPreloader.onLoaded();
+			
+		}
+		
+	}
+
+	public static function getAsset (inName:String):Dynamic {
+		
+		
+		if (inName=="assets/nme.png")
+			 
+            return Assets.getBitmapData ("assets/nme.png");
+         
+		
+		
+		return null;
+		
+	}
+	
+	
+	private static function preloader_onComplete (event:Event):Void {
+		
+		mPreloader.removeEventListener (Event.COMPLETE, preloader_onComplete);
+		
+		nme.Lib.current.removeChild(mPreloader);
+		mPreloader = null;
+		
+		begin ();
+		
+	}
+	
+}
+
+class NME_assets_nme_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+
+
+#else
+
+import Main;
+
+class ApplicationMain {
+	
+	public static function main () {
+		
+		var hasMain = false;
+		
+		for (methodName in Type.getClassFields(Main))
+		{
+			if (methodName == "main")
+			{
+				hasMain = true;
+				break;
+			}
+		}
+		
+		if (hasMain)
+		{
+			Reflect.callMethod (Main, Reflect.field (Main, "main"), []);
+		}
+		else
+		{
+			var instance = Type.createInstance(Main, []);
+			if (Std.is (instance, flash.display.DisplayObject)) {
+				flash.Lib.current.addChild(cast instance);
+			}
+		}
+		
+	}
+
+}
+
+#end
